4afb34ccc2f7f8d7ec6aa47f72f1823ee2c15777
kiminpyo <rladlsvy2448@naver.com>
2022-03-03 21:51:44 +0900

    더보기 페이징 처리 및 Gridcard with antd

--- client/src/Config.js
+++ client/src/Config.js
@@ -4,4 +4,4 @@ export const USER_SERVER = '/api/users';
 //공통으로 쓰이는 API 경로 묶기
 export const API_URL = 'httpS://api.themoviedb.org/3/';
 export const API_KEY = 'a4343b44533bcc8e32bc48b03c241b0e';
-export const IAGME_BASE_URL = 'http://image.tmdb.org/t/p/'
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,43 +1,77 @@
+import React, {useEffect, useState} from 'react';
+import {API_URL, API_KEY, IMAGE_BASE_URL } from '../../../Config';
+import MainImage from './Sections/MainImage';
+import Gridcards from '../commons/GridCards';
+import {Row} from 'antd';
 
-import react, {useEffect, useState} from 'react';
-import {API_URL, API_KEY } from '../../../Config'
-function LandingPage(){
+function LandingPage(props){
 
+  const [Movies, setMovies] = useState([]) 
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0)
+  
   useEffect(() => {
-      //array[]
-  
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+      //array[]
+      fetchMovies(endpoint)
+    }, []) 
 
-    fetch(endpoint)
-    .then(response => response.json())
-     .then(response => {
-       console.log(response)
-     })
-  }, []) 
-  
-    //state에 넣는다
-  /*   .then(response => {
-
-      setMovies([response.results])
-    })
-
-  },[]) */
-
+      const fetchMovies = (endpoint) =>{
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+          console.log(response.results)
+          //...movies가 없을 때는 기존의 state 정보들이 덮어짐. 
+          setMovies([...Movies,...response.results])
+          setMainMovieImage(response.results[0])
+          setCurrentPage(response.page)
+        })
+     
+    }
+ const loadMoreItems = () =>{
+  const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage +1}`;
+  //array[]
+       fetchMovies(endpoint)
+ }
   return(
     
   <div style={{width: '100%', margin: '0'}}>
 
+    {/* Main Image */}
+    {MainMovieImage && 
+      <MainImage image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+      title={MainMovieImage.original_title}
+      text={MainMovieImage.overview}
+      />
+    }
     <div style={{width: '85%', margin: '1rem auto'}}>
      <h2>movies by latest</h2>
      <hr />
+     {/* Movie Grid Card */}
+     {/* gutter = 카드마다 여백 설정 (x,y) */}
+      <Row gutter={[16,16]}>
+        {Movies && Movies.map((movie, index)=>(
+
+          <React.Fragment key={index}>
+                  <Gridcards
+                  image={movie.poster_path ?
+                    `${IMAGE_BASE_URL}w500${movie.poster_path}`: null}
+                  movieId={movie.id}
+                  movieName={movie.original_title}                  
+                  />
+
+          </React.Fragment>
+        ))}
+
+      </Row>
 
     </div>
     <div style={{display:'flex', justifyContent:'center'}}>
-      <button>Load more</button>
+      <button onClick={loadMoreItems}
+      >Load more</button>
     </div>
 
    </div>
   )
 }
-
 export default LandingPage;
